345f81107bb097acc705319859326ab69c856124
Jallysson <jallysson.rocha@stone.com.br>
2020-07-12 00:05:53 -0300

    Add manage

---
+++ management/manage.py
@@ -0,0 +1,32 @@
+import pickle
+import sys
+sys.path.append('..')
+import json
+
+def _load_model(request):
+    return pickle.load(open(
+        'models/' + \
+        request.json.get('typeLearning') + '/' + \
+        request.json.get('model') + '/' +\
+        request.json.get('modelName') + \
+        '.sav', 'rb'
+    ))
+
+def model_info(request):
+    model = _load_model(request)
+
+    return json.loads('{' + str(model) \
+        .replace('SVC(', '"') \
+        .replace(')', '"') \
+        .replace('=', '":"') \
+        .replace(',', '", "') \
+        .replace(' ', '') \
+        .replace("'", '') \
+        .replace('\n', '') + '}'
+    )
+
+def model_predict(request):
+    model = _load_model(request)
+    return str(
+        model.predict([[2., 2.]])
+    )
