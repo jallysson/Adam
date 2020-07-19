e7e682dd250ecb01f8b21d3a27508593d2c71f0d
Jallysson <jallysson.rocha@stone.com.br>
2020-07-19 00:30:13 -0300

    Refactor main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from settings import HOST, PORT
 from flask import Flask, jsonify, request
-from management.manage import model_parameters, model_predict
-import sys
+from management.manage import (
+    model_parameters,
+    model_prediction,
+    model_training
+)
 
 app = Flask(__name__)
 
@@ -9,14 +13,19 @@ app = Flask(__name__)
 def up():
 	return 'DARK UP'
 
+@app.route('/model_training', methods=['POST'])
+def _get_model_training():
+    response = model_training(request)
+    return jsonify(response)
+
 @app.route('/model_parameters', methods=['GET'])
 def _get_model_parameters():
     response = model_parameters(request)
     return jsonify(response)
 
-@app.route('/model_predict', methods=['GET'])
-def _get_model_predict():
-    response = model_predict(request)
+@app.route('/model_prediction', methods=['POST'])
+def _get_model_prediction():
+    response = model_prediction(request)
     return jsonify(response)
 
 app.run(host=HOST, port=PORT, debug=True)
