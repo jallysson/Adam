daaa56d04d1a3f48d29f3d93034c0c9ed9003c5a
Jallysson <jallysson.rocha@stone.com.br>
2020-08-30 15:15:28 -0300

    Refactor end points

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import sys
 from settings import HOST, PORT
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 from management.manage import (
     model_parameters,
     model_prediction,
+<<<<<<< Updated upstream
     model_training
+=======
+    model_training,
+    models,
+    delete_model
+>>>>>>> Stashed changes
 )
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def up():
 	return 'Adam up'
 
 @app.route('/model_training', methods=['POST'])
-def _get_model_training():
+def _post_model_training():
     response = model_training(request)
     return jsonify(response)
 
@@ -24,8 +33,21 @@ def _get_model_parameters():
     return jsonify(response)
 
 @app.route('/model_prediction', methods=['POST'])
-def _get_model_prediction():
+def _post_model_prediction():
     response = model_prediction(request)
     return jsonify(response)
 
+<<<<<<< Updated upstream
+=======
+@app.route('/models', methods=['GET'])
+def _get_models():
+    response = models()
+    return jsonify(response)
+
+@app.route('/delete_model', methods=['POST'])
+def _post_model():
+    response = delete_model(request)
+    return jsonify(response)
+
+>>>>>>> Stashed changes
 app.run(host=HOST, port=PORT, debug=True)
--- management/manage.py
+++ management/manage.py
@@ -3,19 +3,21 @@ sys.path.append('..')
 import json
 import pickle
 import pandas as pd
+from datetime import datetime
 from algorithms.classification.svm import training
 
-def _load_model(request):
+def _load_model(request, version=''):
     return pickle.load(open(
-        'models/{}/{}/{}.sav'.format(
+        'models/{}/{}/{};{}.sav'.format(
             request.json.get('typeLearning'),
             request.json.get('model'),
-            request.json.get('modelName')
+            request.json.get('modelName'),
+            version
         ),'rb')
     )
 
-def model_parameters(request):
-    model = _load_model(request)
+def model_parameters(request, version=''):
+    model = _load_model(request, version)
     request.json['parameters'] = json.loads('{' + str(model) \
         .replace('SVC(', '"') \
         .replace(')', '"') \
@@ -28,6 +30,7 @@ def model_parameters(request):
     return request.json
 
 def model_training(request):
+    version = datetime.now()
     dataset = pd.read_csv(
         'datasets/training/{}.csv'.format(request.json.get('datasetName'))
     ) 
@@ -35,14 +38,15 @@ def model_training(request):
     features = dataset.drop(columns=[request.json.get('targetColumn')]).values
     model = training(features, targets)
     pickle.dump(model, open(
-        'models/{}/{}/{}.sav'.format(
+        'models/{}/{}/{};{}.sav'.format(
             request.json.get('typeLearning'),
             request.json.get('model'),
-            request.json.get('modelName')
+            request.json.get('modelName'),
+            version
         ), 'wb')
     )
     request.json['features'] = list(dataset.columns.values)
-    return model_parameters(request)
+    return model_parameters(request, version)
 
 def model_prediction(request):
     model = _load_model(request)
@@ -51,4 +55,33 @@ def model_prediction(request):
     )
     predictions = model.predict(dataset.values)
     request.json['predictions'] = list(predictions)
+<<<<<<< Updated upstream
     return request.json
+=======
+    return request.json
+
+def models():
+    files = list()
+    for (dirpath, dirnames, filenames) in os.walk('models'):
+        files += [os.path.join(dirpath, file) for file in filenames if '.sav' in file] 
+    models = []
+    for file in files:
+        file = file.split('/')
+        file = {
+            "typeLearning": file[-3],
+            "model": file[-2],
+            "modelName": file[-1].replace('.sav', '').split(';')[0],
+            "version": file[-1].replace('.sav', '').split(';')[1]
+        }
+        models.append(file)
+    return models
+
+def delete_model(request):
+    os.remove('models/{}/{}/{};{}.sav'.format(
+            request.json.get('typeLearning'),
+            request.json.get('model'),
+            request.json.get('modelName'),
+            request.json.get('version')
+    ))
+    return 'File removed'
+>>>>>>> Stashed changes
