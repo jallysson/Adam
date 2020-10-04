cb2dd0e1cbb99d46e06f22c564b81237e86123d4
Jallysson <jallysson.rocha@stone.com.br>
2020-10-03 21:18:13 -0300

    Update manager.py to upload modelos to dropbox and get modelos in dropbox

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='.env')
+
 HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
+
+TOKEN = os.getenv('TOKEN')
 
 REPOSITORY = 'https://github.com/jallysson/Adam'
